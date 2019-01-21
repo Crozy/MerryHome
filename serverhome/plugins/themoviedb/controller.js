ae4a50bd88da5c950786e5ec13e6fdf461b2e336
Crozy <pololeuck@gmail.com>
2019-01-21 17:21:53 +0100

    Ajout recherche série

---
+++ serverhome/plugins/themoviedb/controller.js
@@ -0,0 +1,85 @@
+const request = require('sync-request');
+
+class WikipediaController {
+    
+    constructor(io){
+            this.io = io;
+    }
+    
+    postAction(req, res){
+        switch(req.params.actionId){
+			case "SearchSerie": 
+			var requestUrl="https://api.themoviedb.org/3/search/tv?query=" + req.body.searchValue + "&api_key=829549883362a2a4620637ad10662863&language=fr-FR&page=1";
+			console.log(requestUrl);
+			var wikiReq = request('GET', requestUrl,{cache:'file'});
+			var response = JSON.parse(wikiReq.getBody('utf8'));
+			var lesNoms = "";
+			for(var i= 0; i < response.results.length; i++)
+			{
+     			lesNoms = lesNoms+", "+response.results[i].name;
+			}
+				res.end(JSON.stringify({resultText: lesNoms}));
+			break;
+			case "whois":
+            case "whatis":
+                var requestUrl="https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
+                requestUrl += parseDataSend(req.body.searchValue);
+                console.log(requestUrl);
+                var wikiReq = request('GET', requestUrl,{cache:'file'});
+                var response = JSON.parse(wikiReq.getBody('utf8'));
+                var textResponse= parseDataResponse(response);
+                if(!textResponse){
+                    res.end(JSON.stringify({resultText: "je n'ai pas d'informations"}));
+                }else{
+                    res.end(JSON.stringify({resultText: textResponse}));
+                }
+                break;
+            default:
+                res.end(JSON.stringify({}));
+                break;
+            
+        }
+    }
+}
+
+function parseDataSend(data){
+	if(data.indexOf(" ")){
+		var pieces = data.split(" ");
+		data="";
+		for ( var i in pieces){
+			if(pieces[i].length>3){
+				data += pieces[i].charAt(0).toUpperCase();
+				data += pieces[i].substr(1);
+				if(i!==pieces.length - 1){
+					data+="_";
+				}
+			}
+		}
+	}
+	return data;
+}
+
+function parseDataResponse(response){
+	if(response){
+		if(response.query){
+			for(var i in response.query.pages){
+				if(response.query.pages[i].extract){
+					if(response.query.pages[i].extract.indexOf('\n')!==-1){
+						var textResponse= response.query.pages[i].extract.substr(0, response.query.pages[i].extract.indexOf('\n'));
+					}else{
+						var textResponse= response.query.pages[i].extract;
+					}
+					if(textResponse.length > 300){
+							textResponse= textResponse.substr(0, textResponse.indexOf("."));
+					}
+					console.log(textResponse);
+					return textResponse;
+				}
+			}
+		}
+		console.log(response);
+	}
+	return false;
+}
+
+module.exports = WikipediaController;

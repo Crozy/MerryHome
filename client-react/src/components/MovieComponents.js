67cf281a2f50eb1702e0d8b338c5429f2ae73744
Crozy <pololeuck@gmail.com>
2019-01-22 23:24:16 +0100

    Affiche les images

--- client-react/src/components/MovieComponents.js
+++ client-react/src/components/MovieComponents.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import PluginItem from "./PluginItem"
 
 export default function MovieComponents(props){
    if(props.info != null){
        console.log(props.info);
        //if(response.results != null)
        //console.log(response.results[0]);
+       var lienImage = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
        return <div>
         { props.info.results.map((item, index) => (
+            
             <div key={index}>
                 <h1>{item.title}</h1>
                 <br/>
+                <img src={lienImage + item.poster_path}></img>
                 <h3>Résumé :</h3>
                 <br/>
                 {item.overview}
